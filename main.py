5fae6aa5ddf56f56dc1af272fd5e7c289c550d48
Лаврентьев Роман Владимирович <lavrentevrv@szao.obr.mos.ru>
2025-01-20 13:52:22 +0300

    New feature

--- main.py
+++ main.py
@@ -6,10 +6,10 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 
